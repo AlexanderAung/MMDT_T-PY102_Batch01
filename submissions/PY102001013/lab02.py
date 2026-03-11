dde7b128f4de5c576baa5a9a8b23838eab3d6ee7
Alexander Aung <99577843+AlexanderAung@users.noreply.github.com>
2026-03-11 21:39:31 +0630

    Submit Lab 2 - PY102001013

--- submissions/PY102001013/lab02.py
+++ submissions/PY102001013/lab02.py
@@ -21,6 +21,7 @@ from collections import deque
 # Stack Questions (2)
 # -------------------------
 
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -36,6 +37,30 @@ class Stack:
 
     def pop(self):
         return self.items.pop() if self.items else None
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.items:
+            return None
+        return self.items.pop(0)
+
+    def peek(self):
+        if not self.items:
+            return None
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
+
+    def for_debug(self):
+        return self.items
 
 
 def is_balanced_parentheses(s: str) -> bool:
@@ -85,12 +110,30 @@ def next_greater_to_right(nums: list[int]) -> list[int]:
       output -> [4, 2, 4, -1, -1]
     """
     # TODO: implement using a stack (monotonic stack)
-    raise NotImplementedError
+    stk = Stack()
+    result = [-1 for _ in range(len(nums))]
+
+    # use stack to store
+    for n_left_idx, n_left in enumerate(nums):
+        # 1. store current n_left's value or index in a Stack
+        # 2. iterate the nums again with n_right from the Stack's last value - index + 1
+        # 3. if n_right is greater than n_left
+        # 4. append the n_right's value to the result list
+
+        while stk.size() > 0 and nums[stk.peek()] < n_left:
+            left_idx = stk.peek()
+            stk.pop()
+            result[left_idx] = n_left
+
+        # store current index for future comparison
+        stk.push(n_left_idx)
+    return result
 
 
 # -------------------------
 # Queue Questions (2)
 # -------------------------
+
 
 def first_non_repeating(stream: str) -> str:
     """
@@ -128,21 +171,44 @@ def hot_potato(names: list[str], k: int) -> str:
     Example:
       names = ["A", "B", "C", "D"]
       k = 2
-      1st round: 
+      1st round:
       - "A"--> "B-->C
-      - C is eliminated. 
+      - C is eliminated.
       - Remaining: ["A", "B", "D"]
       - Next HOlder: "D"
       2nd round:
       - "D" --> "A" --> "B"
       - B is eliminated
       - Remaining: ["D", "A"]
-      3rd round: 
+      3rd round:
       - "D"--> "A" --> "D"
-      - D is eliminated. 
+      - D is eliminated.
 
      Winner: "A"
 
     """
     # TODO: implement using a queue (deque)
-    raise NotImplementedError
+    q = Queue()
+
+    for name in names:
+        q.push(name)
+
+    # while size of Q is not 1
+
+    # pop k times
+    # and then get the pop resul and then push back into the q
+    while q.size() != 1:
+        for _ in range(k - 1):
+            get_back_in_line = q.pop()
+            # print("get_back_in_line", get_back_in_line)
+
+            q.push(get_back_in_line)
+
+            # print("q.peek", q.peek())
+
+            # print(q.for_debug())
+            # print("-----FINISH ONE FOR-----")
+        q.pop()
+        # print("--FINISH ONE WHILE RESULT >>> ", q.for_debug())
+    return q.peek()
+    # print(q.peek())
